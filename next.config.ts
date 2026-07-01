8d63cff3d7b302ebd19b92c181f957363690e6be
uedkkk <uedkkkwa@outlook.com>
2026-07-01 21:19:34 +0800

    feat: configure upload directories and Next.js image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 1080, 1920, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
 };
 
-export default nextConfig;
+export default config;
